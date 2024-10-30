08ec78f2995f0c3c2373b151fc1837b703122de2
Theo <ojw1912@gmail.com>
2024-10-30 15:46:53 +0900

    24/10/30 데브옵스 17일차 ~ Javascript 3일차

---
+++ 241030 Javascript 3일차/1-5-조건문/5-quiz2.js
@@ -0,0 +1,40 @@
+let numberA = prompt(`정수 A`)
+let numberB = prompt(`정수 B`)
+let numberC = prompt(`정수 C`)
+
+
+// let Minimum = numberA;
+
+// 1. 1f-else if문 
+// // 가장 작은 수 찾기
+// let min;
+// if (a < b) {
+//   // a가 최소값
+//   if (a < c) {
+//     // a가 최소값
+//     min = a;
+//   } else {
+//     // c가 최소값
+//     min = c;
+//   }
+// } else {
+//   // b가 최소값
+//   if (b < c) {
+//     // b가 최소값
+//     min = b;
+//   } else {
+//     // c가 최소값
+//     min = c;
+//   }
+// }
+
+// 2. 삼항 연산자
+// let Minimum = numberA > numberB ? numberB : numberA;
+// Minimum = numberA > numberC ? numberC : numberA;
+// Minimum = numberB > numberC ? numberC : numberB;
+
+
+console.log(`최소값: ${min}`);
+
+
+alert(`최소값은 ${Minimum}입니다.`)
