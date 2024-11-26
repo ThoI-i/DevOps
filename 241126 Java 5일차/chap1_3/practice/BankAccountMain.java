3aa7d9a640e75bb49ed7f95abd7accde2a2c2ab8
Theo <ojw1912@gmail.com>
2024-11-26 21:49:11 +0900

    24/11/26 데브옵스 36일차 ~ Java 5일차

---
+++ 241126 Java 5일차/chap1_3/practice/BankAccountMain.java
@@ -0,0 +1,28 @@
+//package chap1_3.practice;
+//
+//public class BankAccountMain {
+//
+//    public static void main(String[] args) {
+//
+//        BankAccount pA1 = new BankAccount_ref(001, "pA1", 5000);
+//        BankAccount pB2 = new BankAccount_ref(002, "pB2", 10000);
+//        BankAccount pC3 = new BankAccount_ref(003, "pC3", 20000);
+//
+//
+////        deposite();
+////        withdraw();
+//        pA1.displayBalance();
+//        pB2.displayBalance();
+//        pC3.displayBalance();
+//        System.out.println("===============");
+//        pA1.withdraw(5000);
+//        pB2.withdraw(15000);
+//        pC3.withdraw(15000);
+//        System.out.println("===============");
+//        pA1.deposite(5000);
+//        pB2.deposite(5000);
+//        pC3.deposite(15000);
+//        System.out.println("===============");
+//        pB2.withdraw(15000);
+//    }
+//}
