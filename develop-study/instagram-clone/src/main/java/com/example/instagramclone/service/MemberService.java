1b1aaa532a020503633bb4ecdf949a10c9e2a38e
Theo <ojw1912@gmail.com>
2025-01-08 14:15:43 +0900

    25/01/08 데브옵스 65일차 ~ Spring 18일차 / DB 11일차

---
+++ develop-study/instagram-clone/src/main/java/com/example/instagramclone/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.example.instagramclone.service;
+
+import com.example.instagramclone.domain.member.dto.request.SignUpRequest;
+import com.example.instagramclone.domain.member.entity.Member;
+import com.example.instagramclone.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional // 트랜잭션 처리
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    // 회원가입 중간처리
+    public void signUp(SignUpRequest signUpRequest) {
+
+        // 순수 비밀번호를 꺼내서 암호화
+        String rawPassword = signUpRequest.getPassword();
+        // 암호화된 패스워드
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        // 회원정보를 엔터티로 변환
+        Member newMember = signUpRequest.toEntity();
+        // 패스워드를 인코딩패스워드로 교체
+        newMember.setPassword(encodedPassword);
+
+        // DB에 전송
+        memberRepository.insert(newMember);
+    }
+}
