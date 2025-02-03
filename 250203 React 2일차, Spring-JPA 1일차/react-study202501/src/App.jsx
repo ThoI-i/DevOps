6922fe2bfe2e2d98daa42bc6f58e8907ad8af259
Theo <ojw1912@gmail.com>
2025-02-03 16:32:23 +0900

    25/02/03 데브옵스 79일차 ~ React 2일차, Spring-JPA 1일차[D[C

---
+++ 250203 React 2일차, Spring-JPA 1일차/react-study202501/src/App.jsx
@@ -0,0 +1,31 @@
+import ExpenseList from "./components/expenses/ExpenseList";
+
+// 컴포넌트
+const App = () => {
+
+  // 서버에서 지출항목 JSON이 응답됨
+  const expenses = [
+    {
+      title: '치킨',
+      price: 30000,
+      date: new Date(2025, 2 - 1, 3),
+    },
+    {
+      title: '족발',
+      price: 50000,
+      date: new Date(2024, 9 - 1, 30),
+    },
+    {
+      title: '헬스장등록',
+      price: 300000,
+      date: new Date(2025, 1 - 1, 15),
+    },
+  ];
+
+  return (
+    <ExpenseList expenses={expenses} />
+  );
+}
+
+
+export default App;
