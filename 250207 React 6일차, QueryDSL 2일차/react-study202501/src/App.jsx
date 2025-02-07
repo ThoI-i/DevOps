244e063a4b0ee36befec3d8afe38be7e73f89bda
Theo <ojw1912@gmail.com>
2025-02-07 16:12:14 +0900

    25/02/07 데브옵스 83일차 ~ React 6일차, QueryDSL 2일차

---
+++ 250207 React 6일차, QueryDSL 2일차/react-study202501/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './App.css';
+import MainHeader from './components/SideEffect/MainHeader';
+import Home from './components/SideEffect/Home';
+import Login from './components/SideEffect/Login';
+
+// 컴포넌트
+const App = () => {
+
+
+  // 로그인 상태 체크
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  // 지금 이 컴포넌트가 렌더링될 때 localStorage에서 토큰을 꺼내서 이전에 로그인 했던 사람인지를 확인
+  console.log('토큰 검사!!');
+  const token = localStorage.getItem('login-flag');
+
+  if (token === '1') {
+    setIsLoggedIn(true);
+  }
+  
+
+  // 로그인 수행 함수를 전달
+  const handleLogin = (email, password) => { 
+
+    // 검증
+    if (email === 'abc@def.com' && password === '12345678') {
+      localStorage.setItem('login-flag', '1');
+      setIsLoggedIn(true);
+    } else {
+      alert('로그인 실패!');
+    }
+  };
+
+  return <>
+    <MainHeader />
+    <main>
+      {isLoggedIn && <Home />}
+      {!isLoggedIn && <Login onLogin={handleLogin} />}
+    </main>
+  </>;
+};
+
+export default App;
