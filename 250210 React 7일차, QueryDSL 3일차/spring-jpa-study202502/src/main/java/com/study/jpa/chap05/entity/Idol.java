9cb3a78f68ca8adbef542eb00e698345d59e24b1
Theo <ojw1912@gmail.com>
2025-02-10 19:29:29 +0900

    25/02/10 데브옵스 84일차 ~ React 7일차, QueryDSL 3일차

---
+++ 250210 React 7일차, QueryDSL 3일차/spring-jpa-study202502/src/main/java/com/study/jpa/chap05/entity/Idol.java
@@ -0,0 +1,46 @@
+package com.study.jpa.chap05.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_idol")
+@Setter
+@Getter
+@ToString(exclude = "group")
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Idol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idol_id")
+    private Long id;
+
+    private String idolName;
+
+    private int age;
+
+    private String gender; // 성별 추가
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    public Idol(String idolName, int age, Group group) {
+        this(idolName, age, null, group);
+    }
+    public Idol(String idolName, int age, String gender, Group group) {
+        this.idolName = idolName;
+        this.age = age;
+        this.gender = gender;
+        if (group != null) {
+            changeGroup(group);
+        }
+    }
+
+    public void changeGroup(Group group) {
+        this.group = group;
+        group.getIdols().add(this);
+    }
+}
